b32cf9e8afebc2995290b54e9946284196fb839c
Igor Sergiienko <isergiienko@evolutiongaming.com>
2017-11-29 22:01:38 +0200

    init

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "extends": "airbnb",
+  "parser": "babel-eslint",
+  "env": {
+    "browser": true,
+    "es6": true,
+  },
+  "rules": {
+    "no-console": [2],
+    "max-len": [2, 100],
+  },
+};
---
+++ README.md
@@ -0,0 +1,15 @@
+# Compomemts
+- install node dependencies
+```sh
+  yarn
+```
+
+- run application
+```sh
+  yarn start
+```
+
+- run linter
+```sh
+  yarn lint
+```
